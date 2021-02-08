c8955569bda8ff947014bfc4de99a1a6e4fb1917
Hanzel Gotia <hanzelgotia@gmail.com>
2021-02-08 09:56:25 -1000

    Added StringMethods with Camelcase and PascalCase classes

---
+++ CamelCase/StringMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CamelCase
+{
+    public class StringMethods
+    {
+        // turns a string of words into camelCase
+        public static string camelCase(string inputString)
+        {
+            string outputstring = "";
+            string[] splitWord = inputString.Split(' '); // word, word, word
+
+            // turn first letter into lower case
+            string firstLetterFirstWord = splitWord[0][0].ToString().ToLower();
+
+            // capitalize all the first letters of words after the first word
+            for (int i = 1; i < splitWord.Length; i++)
+            {
+                outputstring += splitWord[i][0].ToString().ToUpper();
+                outputstring += splitWord[i].Substring(1, splitWord[i].Length - 1);
+            }
+            // first letter of first word + the rest of the first word + all the other words with capitalized first letters
+            return firstLetterFirstWord + splitWord[0].Substring(1,splitWord[0].Length-1) + outputstring; 
+        }
+
+        // turns a string of words into PascalCase
+        public static string PascalCase(string inputString)
+        {
+            string outputstring = "";
+            string[] splitWord = inputString.Split(' '); // word, word, word
+
+            for (int i = 0; i < splitWord.Length; i++)
+            {
+                outputstring += splitWord[i][0].ToString().ToUpper();
+                outputstring += splitWord[i].Substring(1, splitWord[i].Length - 1);
+            }
+            return  outputstring;
+        }
+    }
+}
---
+++ CamelCaseTest/UnitTest1.cs
@@ -0,0 +1,46 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CamelCase;
+
+namespace CamelCaseTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CamemlCase_youWinSome()
+        {
+            Assert.AreEqual("youWinSome", StringMethods.camelCase("You win some"));;
+        }
+
+        [TestMethod]
+        public void CamemlCase_quickbrownfox()
+        {
+            Assert.AreEqual("theQuickBrownFoxJumpsOverTheLazyDog", StringMethods.camelCase("the quick brown fox jumps over the lazy dog")); ;
+        }
+
+        [TestMethod]
+        public void CamemlCase_OneWord()
+        {
+            Assert.AreEqual("one", StringMethods.camelCase("One")); ;
+        }
+
+        [TestMethod]
+        public void PascalCase_youWinSome()
+        {
+            Assert.AreEqual("YouWinSome", StringMethods.PascalCase("You win some")); ;
+        }
+
+        [TestMethod]
+        public void PascalCase_quickbrownfox()
+        {
+            Assert.AreEqual("TheQuickBrownFoxJumpsOverTheLazyDog", StringMethods.PascalCase("the quick brown fox jumps over the lazy dog")); ;
+        }
+
+        [TestMethod]
+        public void PascalCase_OneWord()
+        {
+            Assert.AreEqual("One", StringMethods.PascalCase("one")); ;
+        }
+
+    }
+}
