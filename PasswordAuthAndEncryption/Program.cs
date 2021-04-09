9748ca14e70d65dcc8c0ee8e4227796b1a1bf581
Hanzel Gotia <hanzelgotia@gmail.com>
2021-04-09 00:45:11 -0400

    Added PasswordAndEncryptionAuthentication

---
+++ PasswordAuthAndEncryption/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace PasswordAuthAndEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetSelection();
+        }
+
+        public static void Authenticate()
+        {
+            Console.Write("\nEnter a username: ");
+            string username = Console.ReadLine();
+
+            foreach (var user in UserAccounts.Keys)
+            {
+                if (username == user)
+                {
+                    CheckPassword(user);
+                }
+            }
+            Console.WriteLine("Username not found. Try again.");
+            Authenticate();
+        }
+
+        public static void CheckPassword(string user)
+        {
+            Console.Write("\nEnter your password: ");
+            string password = Encrypt(Console.ReadLine());
+
+            if (password == UserAccounts[user])
+            {
+                Console.WriteLine("\n\n*** CONGRATULATIONS! User and password matched! ***\n");
+                GoMainMenu();
+            }
+            else
+            {
+                Console.WriteLine("\nPassword mismatch. Try again.");
+                CheckPassword(user);
+            }
+        }
+        public static string Encrypt(string password)
+        {
+            int shift = 10;
+            string shiftedPassword = "";
+
+            foreach (var item in password)
+            {
+                int shiftedItem = item + shift;
+
+                if (shiftedItem > 127)
+                     shiftedItem -= 95;
+
+                shiftedPassword += (char)shiftedItem;
+            }
+            return shiftedPassword;
+        }
+        public static void GetSelection()
+        {
+            Console.WriteLine("\n--------------------------------------");
+            Console.WriteLine("PASSWORD AUTHENTICATION SYSTEM\n");
+            Console.WriteLine("Please select one option:");
+            Console.WriteLine("1. Create an account\n2. Authenticate a user and password\n3. Exit the system");
+            Console.WriteLine("--------------------------------------");
+
+            int selection = GetUserInput();
+            
+            if (selection == 1)
+                SignUp();
+            else if (selection == 2)
+                Authenticate();
+            else if (selection == 3)
+                System.Environment.Exit(0);
+        }
+        public static void GoMainMenu()
+        {
+            Console.WriteLine("\nTaking you back to the Main Menu...");
+            Thread.Sleep(2 * 1000);
+
+            GetSelection();
+        }
+        public static int GetUserInput()
+        {
+            Console.Write("\nEnter selection: ");
+            string selection = Console.ReadLine();
+            int inputValue;
+            bool success = int.TryParse(selection, out inputValue);
+
+            while (!success || 
+                    inputValue == 0 ||
+                    inputValue < 1 ||
+                    inputValue > 3)
+            {
+                Console.WriteLine("Error! Enter a valid selection!");
+                Console.Write("\nEnter selection: ");
+                selection = Console.ReadLine();
+                success = int.TryParse(selection, out inputValue);
+            }
+
+            return inputValue;
+        }
+        public static void SignUp()
+        {
+            Console.Write("\nEnter a username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Enter a password: ");
+            string password = Console.ReadLine();
+            string encryptedPassword = Encrypt(password);
+
+            Console.WriteLine("\n*** ENCRYPTING PASSWORD ***");
+            Thread.Sleep(1 * 1000);
+            Console.WriteLine($"Encrypted password is {encryptedPassword}");
+
+            UserAccounts.Add(username, encryptedPassword);
+            GoMainMenu();
+        }
+        static Dictionary<string, string> UserAccounts = new Dictionary<string, string>();
+    }
+}
---
+++ ValidateIntInput/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ValidateIntInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getAndValidateInput();
+        }
+
+        private static int getAndValidateInput()
+        {
+            Console.Write($"Enter an integer: ");
+
+            string input = Console.ReadLine();
+            int inputValue;
+            bool success = int.TryParse(input, out inputValue);
+
+            while (!success || inputValue == 0)
+            {
+                Console.WriteLine("Invalid Input. Try again!");
+                Console.Write($"Enter a valid number :");
+                input = Console.ReadLine();
+                success = int.TryParse(input, out inputValue);
+            }
+
+            return inputValue;
+        }
+    }
+}                                                              
