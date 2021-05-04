d208589b20892555d636e42d5ce63eb361745ab9
Hanzel Gotia <hanzelgotia@gmail.com>
2021-05-04 13:56:18 -0400

    Added IntReverse

---
+++ IntReverse/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace IntReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = -12345;
+            int number2 = 998811;
+            int number3 = -100;
+
+            Console.WriteLine(ReverseInt(number));
+            Console.WriteLine(ReverseInt(number2));
+            Console.WriteLine(ReverseInt(number3));
+
+            Console.WriteLine();
+
+            Console.WriteLine(ReverseInt(number));
+            Console.WriteLine(Switcheroo(number2));
+            Console.WriteLine(Switcheroo(number3));
+        }
+
+        private static int Switcheroo(int number)
+        {
+            string output = "";
+
+            if (number < 0)
+                output = "-";
+
+            number = Math.Abs(number);
+
+            while(number > 0)
+            {
+                output += number % 10;
+                number /= 10;
+            }
+            return int.Parse(output);
+        }
+
+        private static int ReverseInt(int number)
+        {
+            string output = "";
+            
+            if (number < 0)
+                output = "-";
+
+            string strNumber = Math.Abs(number).ToString();
+
+            for (int i = strNumber.Length - 1; i >= 0; i--)
+                output += strNumber[i];
+
+            return int.Parse(output);
+        }
+    }
+}
--- UniqueInArray/Program.cs
+++ UniqueInArray/Program.cs
@@ -7,14 +7,51 @@ namespace UniqueInArray
     {
         static void Main(string[] args)
         {
+            //int[] array = { 5, 3, 4, 2, 1 };
+            //ArraySort(array);
+            //FindUnique();
+            RemoveVowels();
+        }
+
+        private static void RemoveVowels()
+        {
+            string troll = "traioulloooll";
+
+
+
+            Console.WriteLine(troll);
+        }
+
+        private static int[] ArraySort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    int temp = 0;
+
+                    if (array[i] > array[j])
+                    {
+                        temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        private static void FindUnique()
+        {
             int[] items = { 1, 2, 3, 4, 3, 2, 1 };
-            int n = items.Length;
+
             List<int> uniqueNumbers = new List<int>();
             bool isUnique = true;
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < items.Length; i++)
             {
-                for (int j = 1; j < n; j++)
+                for (int j = 0; j < i; j++)
                 {
                     if (items[i] == items[j])
                     {
@@ -27,42 +64,34 @@ namespace UniqueInArray
                 {
                     uniqueNumbers.Add(items[i]);
                 }
-
             }
 
             foreach (var item in uniqueNumbers)
             {
                 Console.WriteLine(item);
             }
+        }
 
+        private static void UniqueFinder()
+        {
+            int[] testarray = { 1, 2, 3, 2, 1 };
+            List<int> uniqueValues = new List<int>();
+            bool isUnique = true;
 
+            for (int i = 0; i < testarray.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (testarray[i] == testarray[j])
+                    {
+                        isUnique = false;
+                        break;
+                    }    
 
-            //int[] items = { 1, 2, 3, 4, 3, 2, 1 };
-            //int n = items.Length;
-            //List<int> uniqueItems = new List<int>();
+                    if (isUnique) uniqueValues.Add(testarray[i]);
 
-            //for (int i = 0; i < n; i++)
-            //{
-            //    bool isDuplicate = false;
-            //    for (int j = 0; j < i; j++)
-            //    {
-            //        if (items[i] == items[j])
-            //        {
-            //            isDuplicate = true;
-            //            break;
-            //        }
-            //    }
-
-            //    if (!isDuplicate)
-            //    {
-            //        uniqueItems.Add(items[i]);
-            //    }
-            //}
-
-            //foreach (var item in uniqueItems)
-            //{
-            //    Console.WriteLine(item);
-            //}
+                }
+            }
         }
     }
 }
